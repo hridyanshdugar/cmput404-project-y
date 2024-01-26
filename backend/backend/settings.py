6ba5b3fed1825b0862e0f548c2e6b6465df51063
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-01-26 16:02:12 -0700

    feat: add next.js and docker compose

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1cp7#$jrd4oyg#f(ou_tn8#0$-v!5r$vyrnvqmy5(s!g6k3@m6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
