1666098ca7b649ea8277654a35e760213b847b0c
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-01-26 17:45:26 -0700

    feat: prefix backend url's with api

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('api/admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
+# Add 'prefix' to all urlpatterns
+urlpatterns = [path(r'^api/', include(urlpatterns))]
