c08c98847ae5bb23beb1c7aecf26002adcb40b36
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-02-02 22:38:36 -0700

    feat: add sidenavbar (#55)
    * feat: add side bar
    
    * f

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,21 @@
+"use client"
+import { Inter } from "next/font/google";
+import style from "./page.module.css";
+import SideBar from "@/components/sidebar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+      <html lang="en">
+          <body className={style.backgroundColer}>
+              <SideBar/>
+              {children}
+          </body>
+    </html>
+  );
+}
---
+++ frontend/src/components/sidebar.tsx
@@ -0,0 +1,46 @@
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import style from './sidebar.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faBell } from '@fortawesome/free-solid-svg-icons'
+import { faPerson } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faPen } from '@fortawesome/free-solid-svg-icons'
+import { faEllipsis } from '@fortawesome/free-solid-svg-icons'
+
+
+export default function SideBar() {
+    return <nav className={style.sidenav}>
+        <ul>
+            <li><a className={style.logo} href="#">𝕐</a></li>
+            <li><a href="#"><div className={style.inline}><FontAwesomeIcon icon={faHome} inverse fixedWidth /></div><div className={[style.inline, style.inlinetitle].join(' ')}>Home</div></a></li>
+            <li><a href="#"><div className={style.inline}><FontAwesomeIcon icon={faSearch} inverse fixedWidth/></div><div className={[style.inline, style.inlinetitle].join(' ')}>Explore</div></a></li>
+            <li><a href="#"><div className={style.inline}><FontAwesomeIcon icon={faBell} inverse fixedWidth/></div><div className={[style.inline, style.inlinetitle].join(' ')}>Notifications</div></a></li>
+            <li><a href="#"><div className={style.inline}><FontAwesomeIcon icon={faEnvelope} inverse fixedWidth/></div><div className={[style.inline, style.inlinetitle].join(' ')}>Messages</div></a></li>
+            <li><a href="#"><div className={style.inline}><FontAwesomeIcon icon={faPerson} inverse fixedWidth/></div><div className={[style.inline, style.inlinetitle].join(' ')}>Profile</div></a></li>
+            <li><a className={style.post} href="#">
+                <div className={style.postButtonBig}><button>Post</button></div>
+                <div className={style.postButtonSmall}><button><FontAwesomeIcon icon={faPen} inverse fixedWidth /></button></div>
+            </a></li>
+        </ul>
+        <div className={style.avatarBottom}>
+        <a href="#">
+                <div className={style.avatarImage}>
+                </div>
+                <div className={style.myName}>
+                    <div>
+                        John Dowe
+                    </div>
+                    <div className={style.atNameText}>
+                        @beacon
+                    </div>
+                </div>
+                <div className={style.dotdotdoticon}>
+                    <FontAwesomeIcon icon={faEllipsis} inverse fixedWidth/>
+                </div>
+        </a>
+        </div>              
+  
+</nav>;
+}
