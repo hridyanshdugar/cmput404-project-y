6687860cf8f724bc4c10997a07b204229decbb89
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-02-24 16:14:28 -0700

    feat: add post componenent (#59)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,4 +2,14 @@ module.exports = {
     compiler: {
         styledComponents: true,
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+                port: '',
+                pathname: '**',
+            },
+        ],
+    }
 }
--- frontend/src/components/singlepost.tsx
+++ frontend/src/components/singlepost.tsx
@@ -1,13 +1,56 @@
+"use client";
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import style from './singlepost.module.css'
+import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome } from '@fortawesome/free-solid-svg-icons'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import { faBell } from '@fortawesome/free-solid-svg-icons'
-import { faPerson } from '@fortawesome/free-solid-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faPen } from '@fortawesome/free-solid-svg-icons'
 import { faEllipsis } from '@fortawesome/free-solid-svg-icons'
+import { faArrowUpFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { faRepeat } from '@fortawesome/free-solid-svg-icons'
+import { faComment } from '@fortawesome/free-regular-svg-icons'
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
+import React from 'react'
+import { Card } from 'react-bootstrap';
+
+export function TimeConverter(date: Date) {
+    var now  = new Date();
+    var seconds = (now.getTime() - date.getTime()) / 1000;
+    var minutes =  (now.getTime() - date.getTime()) / 1000/60;
+    var hours =  (now.getTime() - date.getTime()) / 1000/60/24;
+    if (seconds <= 60) {
+        return (
+            <>
+              {Math.round(seconds)}s
+            </>
+          )
+    } else if (minutes <= 60) {
+        return (
+            <>
+              {Math.round(minutes)}m
+            </>
+          )
+    } else if (hours <= 24) {
+        return (
+            <>
+              {Math.round(hours)}h
+            </>
+          )
+    } else {
+        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        if (date.getFullYear() === now.getFullYear()) {
+            return (
+                <>
+                    {months[date.getMonth()]} {date.getDay()}
+                </>
+              )
+        }
+        return (
+            <>
+                {months[date.getMonth()]} {date.getDay()}, {date.getFullYear()}
+            </>
+          )
+    }
+  }
+  
 
 type Props = {
     name: string;
@@ -22,44 +65,57 @@ type Props = {
     postID: number;
 };
 
-export default function SinglePost(props: Props) {
-    return <div>
-        <div>{ props.profileImage }</div>
-        <div>
-            <div>
-                <div>
-                    {props.name}
+export default class SinglePost extends React.Component<Props> {
+        constructor(props: Props) {
+          super(props);
+        }    
+    
+    render() {
+        const date = new Date(0);
+        date.setUTCSeconds(this.props.date);
+        return <div className={style.overflow}>
+            <div className={style.blockImage}><Image className={style.img} src={this.props.profileImage} alt={''} width={40} height={40}/></div>
+            <div className={style.blockContent}>
+                <div className={[style.topText, style.blockFlexContent].join(' ')}>
+                    <div className={style.topLeft}>
+                        <div className={style.inlineBlock}>
+                            {this.props.name}
+                        </div>
+                        <div className={[style.topUserText, style.inlineBlock].join(' ')}>
+                            {this.props.username}
+                        </div>
+                        <div className={[style.topUserText, style.inlineBlock].join(' ')}> · {TimeConverter(date)}</div>                    
+                    </div>
+                    <div className={style.separator}/>
+                    <div>
+                        <FontAwesomeIcon icon={faEllipsis} inverse fixedWidth/>
+                    </div>
+                </div>
+                <div  className={style.topBottom}>
+                    {this.props.text}
                 </div>
                 <div>
-                    {props.username}
+                    {this.props.postImage &&     <Card className="bg-dark text-white">
+      <Card.Img src={this.props.postImage} alt="Card image" />
+    </Card>}
                 </div>
-                <div>
-                    {props.date}
-                </div>
-                <div>
-                    icon
-                </div>                   
+                <div className={style.flexContainer}>
+                    <div  className={style.flexItem}>
+                    <FontAwesomeIcon icon={faComment} fixedWidth/> {this.props.comments}
+                    </div>
+                    <div className={style.flexItem}>
+                    <FontAwesomeIcon icon={faRepeat} fixedWidth/> {this.props.retweets}
+                    </div>
+                    <div className={style.flexItem}>
+                    <FontAwesomeIcon icon={faHeart} fixedWidth/> {this.props.likes}
+                    </div>
+                    <div className={style.separator}></div>
+                    <div className={style.flexItem2}>
+                    <FontAwesomeIcon icon={faArrowUpFromBracket} fixedWidth/>
+                    </div>                   
+                </div>            
             </div>
-            <div>
-                {props.text}
-            </div>
-            <div>
-                {props.postImage}
-            </div>
-            <div>
-                <div>
-                    {props.comments}
-                </div>
-                <div>
-                    {props.retweets}
-                </div>
-                <div>
-                    {props.likes}
-                </div>
-                <div>
-                    share {props.postID}
-                </div>                   
-            </div>            
-        </div>
-    </div>;
+        </div>;
+
+    }
 }
