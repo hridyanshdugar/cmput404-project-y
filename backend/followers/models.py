d9ac56e98134dfc67c3b45791b349021e4039572
Hridyansh Dugar <dugar@ualberta.ca>
2024-02-24 20:06:24 -0700

    Remake the follower and newfollow request table

--- backend/followers/models.py
+++ backend/followers/models.py
@@ -5,10 +5,12 @@ USERNAME_MAX_LENGTH = 100
 URL_MAX_LENGTH = 300
 
 # Create your models here.
-class Followers(models.Model):
+class Follower(models.Model):
     name = models.CharField(max_length=USERNAME_MAX_LENGTH)  # Need to set max user length
+    follower = models.CharField(max_length=USERNAME_MAX_LENGTH)
     url = models.CharField(max_length=URL_MAX_LENGTH)
 
-class NewFollowRequests(models.Model):
+class NewFollowRequest(models.Model):
     name = models.CharField(max_length=USERNAME_MAX_LENGTH)  # Need to set max user length
+    follower = models.CharField(max_length=USERNAME_MAX_LENGTH)
     url = models.CharField(max_length=URL_MAX_LENGTH)
