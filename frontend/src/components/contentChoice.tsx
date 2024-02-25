2db17f41c78e5ef662acaa379a032b8c83764f5d
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-02-25 11:50:52 -0700

    feat: improve home page + post component (#63)

---
+++ frontend/src/components/contentChoice.tsx
@@ -0,0 +1,21 @@
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import style from './contentChoice.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { Card, Form, InputGroup } from 'react-bootstrap'
+
+
+export default function HomeSelector() {
+    return <>
+                <div className={style.blockContent}>
+                <div className={style.flexContainer}>
+            <div className={style.flexItem}>
+                    For you
+                    </div>
+            <div className={style.flexItem}>
+            Following
+                    </div>
+                </div>  
+                </div>  
+    </>;
+}
