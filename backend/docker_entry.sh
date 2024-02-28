50a4e564a5d98e32974516350ea9c04f08d7e433
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-02-28 15:15:55 -0700

    Update docker_entry.sh

--- backend/docker_entry.sh
+++ backend/docker_entry.sh
@@ -3,5 +3,6 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 python3 manage.py spectacular --file schema.yml   
-python3 manage.py migrate   
+python3 manage.py migrate
+python3 manage.py migrate collectstatic
 gunicorn --bind 0.0.0.0:9000 backend.wsgi:application --env SCRIPT_NAME=/api
