ed59950d71f8ac539208d6b3db2ce1343a44008e
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-02-28 03:30:20 -0700

    fix again (#103)

---
+++ backend/docker_entry.sh
@@ -0,0 +1,5 @@
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python3 manage.py migrate   
+gunicorn --bind 0.0.0.0:9000 backend.wsgi:application --env SCRIPT_NAME=/api
