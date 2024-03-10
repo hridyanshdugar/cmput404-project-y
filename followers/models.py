1a05928a198e38683fd9617d7531ca81dcb68fe2
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-03-09 23:48:51 -0700

    feat: a move to try and fix the situation (#129)
    * feat: fix database stuff
    
    * make it better
    
    * make it better
    
    * make it better
    
    * make it better
    
    * make it better
    
    * make it better

--- followers/models.py
+++ followers/models.py
@@ -7,10 +7,10 @@ URL_MAX_LENGTH = 300
 # Create your models here.
 class Follower(models.Model):
     name = models.CharField(max_length=USERNAME_MAX_LENGTH)  # Need to set max user length
-    follower = models.CharField(max_length=USERNAME_MAX_LENGTH, primary_key=True)
+    follower = models.CharField(max_length=USERNAME_MAX_LENGTH)
     followerUrl = models.CharField(max_length=URL_MAX_LENGTH)
 
 class NewFollowRequest(models.Model):
     name = models.CharField(max_length=USERNAME_MAX_LENGTH)  # Need to set max user length
-    follower = models.CharField(max_length=USERNAME_MAX_LENGTH, primary_key=True)
+    follower = models.CharField(max_length=USERNAME_MAX_LENGTH)
     followerUrl = models.CharField(max_length=URL_MAX_LENGTH)
