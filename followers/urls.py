1373868ca06802f0c078b5a22896bd9f83b11023
usmanov@ualberta.ca <usmanov@ualberta.ca>
2024-03-26 07:23:07 -0600

    TEST

--- followers/urls.py
+++ followers/urls.py
@@ -5,7 +5,7 @@ app_name = 'followers'
 urlpatterns = [
     path("all/<str:author_id>/followers/<str:follower_id>/", AllFollowerView.as_view()),
     path("<str:author_id>/followers/<str:follower_id>/", FollowerView.as_view()),
-    path("<str:author_id>/followers/", views.getFollowers, name= "/authors/\{AUTHOR_ID\}/followers"),
+    path("<str:author_id>/followers/", views.getFollowers),
     path("<str:author_id>/requests/", views.getNewFollowRequests, name="getNewFollowRequests"),
     path("get/friends", views.getFriends, name= "getFriends"), 
     path("<str:author_id>/decline/<str:follower_id>/", views.declineFollowRequest, name= "declineFollowRequest"),
