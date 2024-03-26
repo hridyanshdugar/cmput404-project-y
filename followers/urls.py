de4da56783b0a6ba4200fc86a76a784e229ec480
usmanov@ualberta.ca <usmanov@ualberta.ca>
2024-03-26 05:03:00 -0600

    TEST

--- followers/urls.py
+++ followers/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, re_path
 from . import views
-from .views import FollowerView
+from .views import FollowerView, AllFollowerView
 app_name = 'followers'
 urlpatterns = [
     re_path(r'^(?P<author_id>.+)/followers/(?P<follower_id>.+)/$', FollowerView.as_view(), name= "/authors/{AUTHOR_ID}/followers/{FOREIGN_AUTHOR_ID}"),
-    re_path(r'^all/(?P<author_id>.+)/followers/(?P<follower_id>.+)/$', FollowerView.as_view(), name= "/authors/all/{AUTHOR_ID}/followers/{FOREIGN_AUTHOR_ID}"),
+    re_path(r'^all/(?P<author_id>.+)/followers/(?P<follower_id>.+)/$', AllFollowerView.as_view(), name= "/authors/all/{AUTHOR_ID}/followers/{FOREIGN_AUTHOR_ID}"),
     path("<str:author_id>/followers/", views.getFollowers, name= "/authors/\{AUTHOR_ID\}/followers"),
     path("<str:author_id>/requests/", views.getNewFollowRequests, name="getNewFollowRequests"),
     path("get/friends", views.getFriends, name= "getFriends"), 
