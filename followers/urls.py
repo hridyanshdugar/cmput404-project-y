64d8922ead0f58da9ab44726a979c3af848faa47
usmanov@ualberta.ca <usmanov@ualberta.ca>
2024-03-26 06:05:17 -0600

    EST

--- followers/urls.py
+++ followers/urls.py
@@ -3,8 +3,8 @@ from . import views
 from .views import FollowerView, AllFollowerView
 app_name = 'followers'
 urlpatterns = [
-    re_path(r'^all/(?P<author_id>.+)/followers/(?P<follower_id>.+)/$', AllFollowerView.as_view(), name= "/authors/all/{AUTHOR_ID}/followers/{FOREIGN_AUTHOR_ID}"),
-    re_path(r'^(?P<author_id>.+)/followers/(?P<follower_id>.+)/$', FollowerView.as_view(), name= "/authors/{AUTHOR_ID}/followers/{FOREIGN_AUTHOR_ID}"),
+    path("all/<str:author_id>/followers/<str:follower_id>/", AllFollowerView.as_view()),
+    path("<str:author_id>/followers/<str:follower_id>/", FollowerView.as_view()),
     path("<str:author_id>/followers/", views.getFollowers, name= "/authors/\{AUTHOR_ID\}/followers"),
     path("<str:author_id>/requests/", views.getNewFollowRequests, name="getNewFollowRequests"),
     path("get/friends", views.getFriends, name= "getFriends"), 
