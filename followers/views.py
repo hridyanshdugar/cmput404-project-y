85560ed159ab8545ab891cbb42c15832ea902458
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-04-06 20:55:26 -0600

    hi

--- followers/views.py
+++ followers/views.py
@@ -163,11 +163,11 @@ class FollowerView(APIView):
     def put(self, request, author_id, follower_id):
         data = json.loads(request.body)
         print("boblb", data)
-        res = requests.request(method="POST", url=request.data["actor"]["host"] + "api/authors/" + str(author_id) + "/inbox/",data=request.body)
+        res = requests.request(method="POST", url=request.data["actor"]["host"] + "api/authors/" + str(follower_id) + "/inbox/",data=request.body)
         if res.status_code == 200:
             req = get_object_or_404(FollowStatus,actor__id=follower_id,obj__id=author_id)
             hi_user = User.objects.get(id=follower_id)
-            inbox = Inbox.objects.get_or_create(author=hi_user)[0]
+            inbox = Inbox.objects.get_or_create(author=data["object"])[0]
             inbox.followRequest.remove(req)
             inbox.save()            
             if data["accepted"]:
