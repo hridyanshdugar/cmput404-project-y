29feddf41e6fc1154074c1bf837d32880df2684c
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-04-09 19:16:47 -0600

    hi

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -55,7 +55,7 @@ const Profile: React.FC<Props> = (props: Props) => {
 		if (request) {
 			let actor = {
 				type: "author",
-				id: user["id"],
+				id: user["host"] + "api/authors/" + user["id"],
 				url: user["url"],
 				host: user["host"],
 				displayName: user["displayName"],
@@ -64,7 +64,7 @@ const Profile: React.FC<Props> = (props: Props) => {
 			};
 			let object = {
 				type: "author",
-				id: props.userid,
+				id: props.host + "api/authors/" + props.userid,
 				host: props.host,
 				displayName: props.name,
 				url: props.url,
